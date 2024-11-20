7a9b1a010706dd5d9147aeac248360738e60e38e
KarunagaranR <110883321+KarunagaranR@users.noreply.github.com>
2024-11-20 15:00:11 +0530

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,18 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        double num1 = 10.5;
+        double num2 = 5.5;
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
